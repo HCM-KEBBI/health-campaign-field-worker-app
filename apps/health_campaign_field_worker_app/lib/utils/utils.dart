d113ceeea37e22fe5eb3b5f4e6234f2fa641a8f8
Naveen Renati <naveen.renati@egovernments.org>
2024-08-20 12:18:04 +0530

    added uniqueId generation with deviceId and other combinations

--- apps/health_campaign_field_worker_app/lib/utils/utils.dart
+++ apps/health_campaign_field_worker_app/lib/utils/utils.dart
@@ -653,3 +653,48 @@ dynamic getValueByKey(List<Map<String, dynamic>> data, String key) {
 
   return null; // Key not found
 }
+
+class UniqueIdGeneration {
+  Future<String> generateUniqueId(
+      String localityCode,
+      String loggedInUserId,
+      ) async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+
+    // Get the Android ID
+    String androidId = androidInfo.serialNumber == 'unknown'
+        ? androidInfo.id.replaceAll('.', '')
+        : androidInfo.serialNumber;
+
+    // Get current timestamp
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+
+    // Combine the Android ID with the timestamp
+    String input = '$loggedInUserId$androidId$localityCode$timestamp';
+
+    // Generate SHA-256 hash
+    List<int> bytes = utf8.encode(input);
+    Digest sha256Hash = sha256.convert(bytes);
+
+    // Convert the hash to a 12-character string and make it uppercase
+    String hashString = sha256Hash.toString();
+    String uniqueId = hashString.substring(0, 12).toUpperCase();
+
+    // Add a hyphen every 4 characters, except the last
+    String formattedUniqueId = uniqueId.replaceAllMapped(
+      RegExp(r'.{1,4}'),
+          (match) => '${match.group(0)}-',
+    );
+
+    // Remove the last hyphen
+    formattedUniqueId =
+        formattedUniqueId.substring(0, formattedUniqueId.length - 1);
+
+    if (kDebugMode) {
+      print('uniqueId : $formattedUniqueId');
+    }
+
+    return formattedUniqueId;
+  }
+}
