86213b0693a4d8e3557717da0913a905daf5fc76
amitverma-egov <amit.verma@egovernments.org>
2024-09-05 17:32:10 +0530

    hide edit individual button if deliver intervention done for individual

--- apps/health_campaign_field_worker_app/lib/widgets/member_card/member_card.dart
+++ apps/health_campaign_field_worker_app/lib/widgets/member_card/member_card.dart
@@ -142,31 +142,40 @@ class MemberCard extends StatelessWidget {
                   ),
                 ],
               ),
-              Positioned(
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: DigitIconButton(
-                    onPressed: () => DigitActionDialog.show(
-                      context,
-                      widget: ActionCard(
-                        items: [
-                          ActionCardModel(
-                            icon: Icons.edit,
-                            label: localizations.translate(
-                              i18.memberCard.editIndividualDetails,
+              (tasks ?? [])
+                          .where(
+                            (element) =>
+                                element.status ==
+                                Status.administeredSuccess.toValue(),
+                          )
+                          .lastOrNull ==
+                      null
+                  ? Positioned(
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: DigitIconButton(
+                          onPressed: () => DigitActionDialog.show(
+                            context,
+                            widget: ActionCard(
+                              items: [
+                                ActionCardModel(
+                                  icon: Icons.edit,
+                                  label: localizations.translate(
+                                    i18.memberCard.editIndividualDetails,
+                                  ),
+                                  action: editMemberAction,
+                                ),
+                              ],
                             ),
-                            action: editMemberAction,
                           ),
-                        ],
+                          iconText: localizations.translate(
+                            i18.memberCard.editDetails,
+                          ),
+                          icon: Icons.edit,
+                        ),
                       ),
-                    ),
-                    iconText: localizations.translate(
-                      i18.memberCard.editDetails,
-                    ),
-                    icon: Icons.edit,
-                  ),
-                ),
-              ),
+                    )
+                  : const Offstage(),
             ],
           ),
           SizedBox(
