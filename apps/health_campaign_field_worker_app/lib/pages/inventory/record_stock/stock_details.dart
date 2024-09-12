598be8d7542f6eae4b1ea32a36dd0323b90a57dc
amitverma-egov <amit.verma@egovernments.org>
2024-09-12 13:04:16 +0530

    hddf-1286 , set supervisor key default value as empty string

--- apps/health_campaign_field_worker_app/lib/pages/inventory/record_stock/stock_details.dart
+++ apps/health_campaign_field_worker_app/lib/pages/inventory/record_stock/stock_details.dart
@@ -94,6 +94,7 @@ class _StockDetailsPageState extends LocalizedState<StockDetailsPage> {
       ),
       _supervisorKey: FormControl<String>(
         validators: isDistributor ? [Validators.required] : [],
+        value: '',
       ),
       _transactionReasonKey: FormControl<TransactionReason>(),
       _waybillNumberKey: FormControl<String>(
