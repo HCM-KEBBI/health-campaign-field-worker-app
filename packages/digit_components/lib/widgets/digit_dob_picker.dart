72fc202260f46525aad8c496ad1d65b0c239c4b9
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2025-01-30 10:37:44 +0530

    commented code removed

--- packages/digit_components/lib/widgets/digit_dob_picker.dart
+++ packages/digit_components/lib/widgets/digit_dob_picker.dart
@@ -166,31 +166,23 @@ class DobValueAccessor extends ControlValueAccessor<DateTime, DigitDOBAge> {
 
   @override
   DateTime? viewToModelValue(DigitDOBAge? viewValue) {
-    // if (viewValue == null || (viewValue.years == 0 && viewValue.months == 0)) {
-    //   return null;
-    // } else {
-    //   return (viewValue.years == 0 && viewValue.months == 0) ||
-    //           viewValue.months > 11
-    //       ? null
-    //       : DigitDateUtils.calculateDob(viewValue);
-    // }
-      if (viewValue == null || (viewValue.years == 0 && viewValue.months == 0)) {
-    return null;
-  } else {
-    final months = viewValue.months;
-    final days = DigitDateUtils.yearsMonthsDaysToDays(
-        viewValue.years, viewValue.months, viewValue.days);
+    if (viewValue == null || (viewValue.years == 0 && viewValue.months == 0)) {
+      return null;
+    } else {
+      final months = viewValue.months;
+      final days = DigitDateUtils.yearsMonthsDaysToDays(
+          viewValue.years, viewValue.months, viewValue.days);
 
-    final calculatedDate = DateTime.now().subtract(Duration(days: days));
+      final calculatedDate = DateTime.now().subtract(Duration(days: days));
 
-    return (viewValue.years == 0 && months == 0) || months > 11
-        ? null
-        : DateTime(
-            calculatedDate.year,
-            calculatedDate.month,
-            1,
-          );
-  }
+      return (viewValue.years == 0 && months == 0) || months > 11
+          ? null
+          : DateTime(
+              calculatedDate.year,
+              calculatedDate.month,
+              1,
+            );
+    }
   }
 }
 
