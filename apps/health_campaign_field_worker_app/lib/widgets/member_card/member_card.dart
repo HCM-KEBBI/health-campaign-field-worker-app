7cf174c2a7ef627760797bd6e540ea442a2ab136
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2025-01-30 15:01:26 +0530

    formatted ad per dart lint

--- apps/health_campaign_field_worker_app/lib/widgets/member_card/member_card.dart
+++ apps/health_campaign_field_worker_app/lib/widgets/member_card/member_card.dart
@@ -235,8 +235,12 @@ class MemberCard extends StatelessWidget {
                                           ? Status.beneficiaryRefused.toValue()
                                           : Status.notVisited.toValue(),
                         ),
-                        iconTextColor:isHead?theme.colorScheme.surfaceTint: theme.colorScheme.error,
-                        iconColor: isHead?theme.colorScheme.surfaceTint: theme.colorScheme.error,
+                        iconTextColor: isHead
+                            ? theme.colorScheme.surfaceTint
+                            : theme.colorScheme.error,
+                        iconColor: isHead
+                            ? theme.colorScheme.surfaceTint
+                            : theme.colorScheme.error,
                       ),
                     )
                   : Align(
